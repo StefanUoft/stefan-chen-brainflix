a2b8656df05e808da06b764b182dcd73f5e363a7
Stefan <stefan.chen@mail.utoronto.ca>
2024-10-26 22:35:43 -0400

    create comments scss

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -1,25 +1,26 @@
 import "./CommentItem.scss";
 
-function CommentItem ({name, timestamp,comment}){
-    const formatDate = (timestamp) => {
-        const date = new Date(timestamp);
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const year = date.getFullYear();
-        return `${month}/${day}/${year}`;
-      };
+function CommentItem({ name, timestamp, comment }) {
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  };
 
-      return(
-        <li className="comment">
-          <div className="comment__avatar"></div>
-          <div className="comment__container">
-            <p className="comment__name">{name}</p>
-            <p className="comment__timestamp">{formatDate(timestamp)}</p>
-            <p className="comment__info">{comment}</p>
-          </div>
-        </li>
-
-      )
+  return (
+    <li className="comment">
+      <div className="comment__avatar"></div>
+      <div className="comment__container">
+        <div className="comment__userinfo">
+          <p className="comment__name">{name}</p>
+          <p className="comment__timestamp">{formatDate(timestamp)}</p>
+        </div>
+        <p className="comment__info">{comment}</p>
+      </div>
+    </li>
+  );
 }
 
 export default CommentItem;
